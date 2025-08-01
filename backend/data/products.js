1b4b29fc060d22cb4c01e74a5f6de9c773996b67
asarthak2003 <136921686+asarthak2003@users.noreply.github.com>
2025-08-02 01:13:10 +0530

    Update products.js

--- backend/data/products.js
+++ backend/data/products.js
@@ -4,70 +4,70 @@ const products = [
     name: "Samsung Galaxy S23 Ultra", 
     price: 1249, 
     description: "Samsung Galaxy S23 Ultra 5G, 256GB, Phantom Black", 
-    image: "https://m.media-amazon.com/images/I/71-+0MKV37HL._UF1000,1000_QL80_.jpg" 
+    image: "https://m.media-amazon.com/images/I/71goZuIha-L._UF1000,1000_QL80_.jpg" 
   },
   { 
     id: 2, 
     name: "Apple iPhone 14 Pro", 
     price: 999, 
     description: "Apple iPhone 14 Pro (128 GB) - Deep Purple", 
-    image: "https://m.media-amazon.com/images/I/61bK6PMOC3L._SL1500_.jpg" 
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzP5GrbTHwh8H7A3OJuBajUDuylb-fYQ3-Mg&s" 
   },
   { 
     id: 3, 
     name: "Sony WH-1000XM5 Headphones", 
     price: 349, 
     description: "Industry Leading Noise Cancellation Wireless Headphones", 
-    image: "https://m.media-amazon.com/images/I/61+L0aELfUL._SL1500_.jpg" 
+    image: "https://www.sony.co.in/image/6145c1d32e6ac8e63a46c912dc33c5bb?fmt=pjpeg&wid=330&bgcolor=FFFFFF&bgc=FFFFFF" 
   },
   { 
     id: 4, 
     name: "Dell XPS 13 Laptop", 
     price: 1299, 
     description: "13.4-inch FHD Touch Laptop, 11th Gen Intel i7", 
-    image: "https://m.media-amazon.com/images/I/71Q0m47zFSL._SL1500_.jpg" 
+    image: "https://i.dell.com/is/image/DellContent//content/dam/ss2/product-images/dell-client-products/notebooks/xps-notebooks/xps-13-9300/global-spi/ng/silver/notebook-xps-13-9300-silver-campaign-hero-504x350-ng.psd?fmt=jpg" 
   },
   { 
     id: 5, 
     name: "Canon EOS R7 Camera", 
     price: 1499, 
     description: "Canon EOS R7 Mirrorless Camera with RF-S 18-150mm Lens", 
-    image: "https://m.media-amazon.com/images/I/81Ww0ovA+gL._SL1500_.jpg" 
+    image: "https://m.media-amazon.com/images/I/61QoynZYblL.jpg" 
   },
   { 
     id: 6, 
     name: "Apple MacBook Air M2", 
     price: 1199, 
     description: "13-inch MacBook Air with M2 Chip (2023)", 
-    image: "https://m.media-amazon.com/images/I/71h4blK80SL._SL1500_.jpg" 
+    image: "https://rukminim2.flixcart.com/image/704/844/xif0q/computer/a/w/p/-original-imagfdeqqe6kkuxw.jpeg?q=90&crop=false" 
   },
   { 
     id: 7, 
     name: "Samsung 55-inch 4K QLED TV", 
     price: 899, 
     description: "Samsung QN90B Neo QLED 4K Smart TV", 
-    image: "https://m.media-amazon.com/images/I/81fxNhKYBXL._SL1500_.jpg" 
+    image: "https://m.media-amazon.com/images/I/71oeLYE-BOL._UF1000,1000_QL80_.jpg" 
   },
   { 
     id: 8, 
     name: "Logitech MX Master 3 Mouse", 
     price: 99, 
     description: "Advanced Wireless Mouse with Ultrafast Scrolling", 
-    image: "https://m.media-amazon.com/images/I/61ni3t1ryQL._SL1500_.jpg" 
+    image: "https://rukminim2.flixcart.com/image/704/844/kpbipow0/mouse/r/b/u/mx-master-3-mouse-logitech-original-imag3h2mzrepmhys.jpeg?q=90&crop=false" 
   },
   { 
     id: 9, 
     name: "Sony PlayStation 5 Console", 
     price: 499, 
     description: "PS5 Console Disc Edition, White", 
-    image: "https://m.media-amazon.com/images/I/619BkvKW35L._SL1500_.jpg" 
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNcjAmYPM5w2gAYrXVyStP_R5yX8a0XH-jYw&s" 
   },
   { 
     id: 10, 
     name: "Apple Watch Series 8", 
     price: 399, 
     description: "Apple Watch Series 8 GPS 45mm", 
-    image: "https://m.media-amazon.com/images/I/71xkSWk13lL._SL1500_.jpg" 
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHOKmxL8Y4GyMm_3yn49zCXT19-qCzskgeLA&s" 
   }
 ];
 export default products;
